24572f8681d88331c014a066c65269ddc6f73660
artforme <me.peremy@gmail.com>
2024-02-17 01:38:14 +0300

    create handler

---
+++ internal/handlers/checksAccountDisconnection/checksAccountDisconnection.go
@@ -0,0 +1,25 @@
+package checksAccountDisconnection
+
+import (
+	"github.com/go-chi/render"
+	"log/slog"
+	"net/http"
+)
+
+type Response struct {
+	Resp string `json:"resp"`
+}
+
+func New(log *slog.Logger) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		const op = "handlers.checkAccessibility.New"
+
+		// add to log info
+		log = log.With(
+			slog.String("op", op),
+		)
+		render.JSON(w, r, Response{
+			Resp: "200",
+		})
+	}
+}
