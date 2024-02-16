fe17d2f8b9e7002d3f7c9a60f4ef115a507859bf
artforme <me.peremy@gmail.com>
2024-02-16 20:55:43 +0300

    chore: init todo list

--- cmd/yandex_smart_house/main.go
+++ cmd/yandex_smart_house/main.go
@@ -1,5 +1,19 @@
 package main
 
 func main() {
+	// TODO: create config
 
+	// TODO: setup logger
+
+	// TODO: make simple oauth 2.0 verification
+
+	// TODO: init storage and connect it
+
+	// TODO: test system
+
+	// TODO: init rest api service for yandex smart house
+
+	// TODO: deploy project
+
+	// TODO: test project
 }
