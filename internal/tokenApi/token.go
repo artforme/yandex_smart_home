3cf042d01c454f18e53653335628f90eecd1671d
artforme <me.peremy@gmail.com>
2024-03-18 22:06:20 +0300

    create token creation

---
+++ internal/tokenApi/token.go
@@ -0,0 +1,26 @@
+package tokenApi
+
+import (
+	"fmt"
+	"github.com/dgrijalva/jwt-go"
+	"os"
+	"time"
+)
+
+func GenerateToken(userID string) (string, error) {
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"user_id": userID,
+		"exp":     time.Now().Add(time.Hour * 7 * 24).Unix(),
+	})
+
+	tokenString, err := token.SignedString([]byte(os.Getenv("CLIENT_SECRET")))
+	if err != nil {
+		return "", err
+	}
+
+	return tokenString, nil
+}
+
+func RedirectToProvider(redirectURI string, token string, state string, client_id string, scope string) string {
+	return fmt.Sprintf("%s?code=%s&client_id=%s&state=%s&scope=%s", redirectURI, token, client_id, state, scope)
+}
